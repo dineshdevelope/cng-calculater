d82500a4794890474a2d3173542d0af52d0de5c9
dineshdevelope <dineshindevelope@gmail.com>
2023-10-20 23:30:53 +0530

    added-javascript

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let tripDistance = prompt("Enter Your Trip Distance in K/m");
-if (!tripDistance) {
-  alert("You Enter Wroung Keyword" + " Try Again From First");
+let calculate = confirm("You Want to Calculate Your Fuel Cost?");
+
+if (calculate) {
+  let name = prompt("Enter Your Name ");
+  let tripDistance = prompt("Enter Your Trip Distance in K/m");
+  console.log("Your Trip Distance :" + tripDistance + "K/m");
+  let fuelEfficency = prompt("Enter Your Fuel Efficiency per Liter");
+  console.log("Your Fuel Efficiency :" + fuelEfficency + " Per Liter");
+  let petrolCost = 100;
+  let kmCharge = petrolCost / fuelEfficency;
+  console.log("Fuel Charge Per K/m " + kmCharge);
+  let fuelCostForTrip = tripDistance * kmCharge;
+  console.log("Your Trip Fuel Cost is " + fuelCostForTrip);
+  console.log("Thanks For Visiting My Page" + " " + name);
+} else {
+  alert("Thanks For Comming");
+  console.log("Thanks For Visiting My Page");
 }
-console.log("Your Trip Distance :" + tripDistance + "K/m");
-let fuelEfficency = prompt("Enter Your Fuel Efficiency per Liter");
-if (!fuelEfficency) {
-  alert("You Enter Wroung Keyword" + " Try Again From First");
-}
-console.log("Your Fuel Efficiency :" + fuelEfficency + " Per Liter");
-let petrolCost = 100;
-let kmCharge = petrolCost / fuelEfficency;
-console.log("Fuel Charge Per K/m " + kmCharge);
-let fuelCostForTrip = tripDistance * kmCharge;
-console.log("Your Trip Fuel Cost is " + fuelCostForTrip);
